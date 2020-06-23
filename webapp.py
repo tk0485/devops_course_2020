4fb93a488fa277e113ea27adac628fb4fe8eaf83
tk0485 <tk0485@users.noreply.github.com>
2020-06-23 09:59:51 -0400

    webapp project code

---
+++ webapp.py
@@ -0,0 +1,31 @@
+import time
+
+import redis
+from flask import Flask
+
+
+
+app = Flask(app)
+cache = redis.Redis(host='redis', port=6379)
+
+
+
+def get_hit_count():
+    retries = 5
+	while True:
+	    try:
+		    return cache.incur('hits')
+		except redis.exceptions.ConnectionError as exc:
+		    if retries == 0:
+			    raise exc
+			retries -=1
+			time.sleep(0.5)
+			
+			
+@app.route('/')
+def hello():
+    count = get_hit_count()
+	return 'Hello World! I have been visited {} times.]n'.format(count)
+	
+if app == "__main__":
+    app.run(host="0.0.0.0". debug=True)
